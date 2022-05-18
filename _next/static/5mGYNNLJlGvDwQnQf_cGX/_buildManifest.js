66c8827d90c62e25c766d7923671c3c7a293f491
teamchong <teamchong@users.noreply.github.com>
2022-05-18 09:44:11 +0000

    deploy: 145732354115bdaae8f401c7d252a6e47bac4b6f

---
+++ _next/static/5mGYNNLJlGvDwQnQf_cGX/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b625687ae170e9c2.css","static/chunks/pages/index-22beecfe1fd15eae.js"],"/_error":["static/chunks/pages/_error-2bd663fb8be60af4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
