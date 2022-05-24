2aee1f6cc45d70ed029edf7ef24398a74aa22ee2
teamchong <teamchong@users.noreply.github.com>
2022-05-24 14:59:36 +0000

    deploy: b1ebdd4756dd9cad13d785e4a2776ad79df86641

--- _next/static/ppissF45alYqAAscxQc0w/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/b625687ae170e9c2.css","static/chunks/pages/index-9a6c69ff52a7df9f.js"],"/_error":["static/chunks/pages/_error-2bd663fb8be60af4.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
